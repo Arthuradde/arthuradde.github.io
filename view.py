d1416d6dc2a25ccf1c0a73dd180f816d0ee7ff4a
Arthur <daniela@MTLLMS4H24SQ3F.cbc-rc.ca>
2026-09-03 14:09:30 -0400

    Basic tile and map reading

---
+++ view.py
@@ -0,0 +1,42 @@
+import yaml
+import json
+
+colors = {
+    'BLUE' : '\033[94m',
+    'CYAN' : '\033[96m',
+    'GREEN' : '\033[92m',
+    'YELLOW' : '\033[93m',
+    'RED' : '\033[91m',
+    'PINK' : '\033[95m',
+    'END' : '\033[0m',
+    'BOLD' : '\033[1m',
+    'UNDERLINE' : '\033[4m'
+}
+
+file = open('tiles.yml', 'r')
+tiles = yaml.safe_load(file)['tiles']
+
+
+def legend():
+    for tile in tiles[1:]:
+        print(f"{colors[tile['color']]} {tile['tile']} {colors['END']}   {tile['name']}")
+        
+def printmap(map):
+    print("#" * (len(map['name']) + 4))
+    print(f"# {map['name']} #")
+    print("#" * (len(map['name']) + 4))
+    
+    for line in map['tiles']:
+        for case in line:
+            tile = next((x for x in tiles if x['id'] == case['tile']), tiles[0])
+            print(f"{colors[tile['color']]} {tile['tile']} {colors['END']}", end='')
+        print('')
+        
+    print('--------')
+    legend()
+    
+
+
+file = open('maps/map1.json', 'r')
+map = json.load(file)
+printmap(map)
